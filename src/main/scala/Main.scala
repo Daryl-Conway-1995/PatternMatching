84ade4f5306e42f33cb103276624dd0b842af419
daryl <darylconway1995@gmail.com>
2019-07-08 09:18:56 +0100

    starting of more complex challenges

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,7 @@
+import ProjectEuler.ChallengeOne
+
 object Main extends App{
-  val patternController = new PatternController
-  patternController.matchPattern(123)
-  patternController.matchPattern("123")
-  patternController.matchPattern(6.5)
-  patternController.matchPattern("abc")
-  patternController.matchPattern("abc123")
-  patternController.matchPattern("123abc")
-  patternController.matchPattern(true)
-  patternController.matchPattern("456",456)
-  patternController.matchPattern("abc",456)
-  patternController.matchRegex("[\\d]*".r,"12345")
-  patternController.matchRegex("[A-Z]{3}[0-9]{2}".r,"ABC12")
+
+  val challenge = new ChallengeOne
+  challenge.produceResult
 }
