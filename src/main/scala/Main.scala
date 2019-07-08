9acdb2f2ff7baf19a9e9fa4118b76b052ea779ce
daryl <darylconway1995@gmail.com>
2019-07-08 12:30:19 +0100

    Task 1, 2 & 3 completed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,4 +8,8 @@ object Main extends App{
   println("Challenge Two: ")
   val challenge2 = new ChallengeTwo
   challenge2.produceResult
+
+  println("Challenge Three: ")
+  val challenge3 = new ChallengeThree
+  challenge3.produceResult
 }
---
+++ src/main/scala/ProjectEuler/ChallengeThree.scala
@@ -0,0 +1,20 @@
+package ProjectEuler
+
+//challenge three is to output the largest prime factor of a large number.
+class ChallengeThree {
+  def produceResult:Unit = {
+    var largeNum: Long = 600851475143l
+    var possibleFactor = 3
+    var largestFactor = 0
+    if (largeNum % 2 == 0) largestFactor = 2
+    while (possibleFactor < Math.sqrt(largeNum)){
+      if(largeNum % possibleFactor == 0) {
+        largestFactor = possibleFactor
+        largeNum = largeNum / possibleFactor
+        possibleFactor = 3
+      }else possibleFactor += 2
+    }
+    if (largeNum < largestFactor) println(largestFactor)
+    else println(largeNum)
+  }
+}
